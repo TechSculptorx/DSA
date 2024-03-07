872c0643b12f51436c6a31130dce842abd952fc7
Priyanshu Vanra <priyanshuvanra2@gmail.com>
2024-03-07 22:30:40 +0530

    complete dynamic memory allocation lesson

--- 14.Dynamic Allocation/transposematrix.cpp
+++ 14.Dynamic Allocation/transposematrix.cpp
@@ -2,5 +2,30 @@
 using namespace std;
 
 int main () {
+   int test ;
+	cin >> test;
 
+	while (test > 0) {
+		int n,m;
+    	cin >> n >> m;
+
+    	int **a = new int*[n];
+
+    	for (int i = 0; i < n; i++) {
+        	a[i] = new int[m];
+
+        	for (int j = 0; j < m; j++) {
+        	    cin >> a[i][j];
+       		 }
+    	}
+
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				cout << a[j][i] << " ";
+			}
+			cout << endl;
+		}
+
+		test--;
+	}
 }
